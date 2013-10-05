bed639a9212977c8d41c59104df914d75c0fbed7
Michael Mrowetz <me@michaelmrowetz.com>
2013-10-05 16:47:02 -0400

    knob wired up

--- circuit/www/js/ko-binding-handlers.js
+++ circuit/www/js/ko-binding-handlers.js
@@ -1,15 +1,26 @@
 ko.bindingHandlers.transitionScreen = {
-    init: function(element, valueAccessor) {
-        $(element).toggle(ko.utils.unwrapObservable(valueAccessor()));
-    },
-    update: function(element, valueAccessor) {
-        ko.utils.unwrapObservable(valueAccessor()) ? $(element).fadeIn() : $(element).fadeOut();
-    }
+	init: function(element, valueAccessor) {
+		$(element).toggle(ko.utils.unwrapObservable(valueAccessor()));
+	},
+	update: function(element, valueAccessor) {
+		ko.utils.unwrapObservable(valueAccessor()) ? $(element).fadeIn() : $(element).fadeOut();
+	}
 };
 
 ko.bindingHandlers.knobify = {
-    init: function(element) {
-        // Turn the input into a knob on load
-        $(element).knob(value);
-    }
+	init: function(element, valueAccessor) {
+
+		console.log(ko.utils.unwrapObservable(valueAccessor()));
+		// Turn the input into a knob on load
+		var knob = $(element).knob({
+			'change' : function (v) {
+				var value = valueAccessor();
+				value(v);
+			}
+		});
+		console.log(knob);
+	},
+	update: function(element, valueAccessor) {
+		$(element).val(ko.utils.unwrapObservable(valueAccessor())).trigger('change');
+	}
 };
