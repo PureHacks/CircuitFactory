114d851b25d5ef8401400fea802513008ca78401
Michael Mrowetz <me@michaelmrowetz.com>
2013-10-05 14:24:31 -0400

    pumped up the font size

--- circuit/www/js/ko-binding-handlers.js
+++ circuit/www/js/ko-binding-handlers.js
@@ -1,12 +1,8 @@
-ko.bindingHandlers.fadeVisible = {
+ko.bindingHandlers.transitionScreen = {
     init: function(element, valueAccessor) {
-        // Initially set the element to be instantly visible/hidden depending on the value
-        var value = valueAccessor();
-        $(element).toggle(ko.utils.unwrapObservable(value)); // Use "unwrapObservable" so we can handle values that may or may not be observable
+        $(element).toggle(ko.utils.unwrapObservable(valueAccessor()));
     },
     update: function(element, valueAccessor) {
-        // Whenever the value subsequently changes, slowly fade the element in or out
-        var value = valueAccessor();
-        ko.utils.unwrapObservable(value) ? $(element).fadeIn() : $(element).fadeOut();
+        ko.utils.unwrapObservable(valueAccessor()) ? $(element).fadeIn() : $(element).fadeOut();
     }
 };
