4c746239aec80a908e3f60db56bd0a0ca481df8e
Michael Mrowetz <me@michaelmrowetz.com>
2013-10-05 15:47:50 -0400

    pre merge wip - work on models

--- circuit/www/js/viewModels/viewModels.js
+++ circuit/www/js/viewModels/viewModels.js
@@ -4,6 +4,20 @@
 
 		self.activeScreen = ko.observable("circuit-home");
 
+		self.currentCircuit = ko.observable();
+		self.savedCircuits = ko.observableArray();
+
+
+
+		self.addNewCircutes = function(id){
+			self.currentCircuit(new Circuit());
+		};
+
+
+		self.startCircuit = function(){
+			
+		};
+
 
 		self.changeScreen = function(id){
 			self.activeScreen(id);
