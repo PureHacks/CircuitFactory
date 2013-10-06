ed4c5a471e3abf39e86960f75186e0951b110331
Michael Mrowetz <me@michaelmrowetz.com>
2013-10-06 18:06:26 -0400

    performace debug

--- circuit/www/js/ko-binding-handlers.js
+++ circuit/www/js/ko-binding-handlers.js
@@ -11,21 +11,23 @@ ko.bindingHandlers.knobify = {
 	init: function(element, valueAccessor) {
 		// Turn the input into a knob on load
 		var knob = $(element).knob({
-            'min': 0,
-            'max': 60,
-            'step': 6,
-            'width': 300,
-            'height': 300,
-            'fgColor': '#0bc9f0',
-            'bgColor': '#fff',
-            'font': 'Raleway Dots',
+			'min': 0,
+			'max': 60,
+			'step': 6,
+			'width': 300,
+			'height': 300,
+			'fgColor': '#0bc9f0',
+			'bgColor': '#fff',
+			'font': 'Raleway Dots',
 			'change' : function (v) {
-				var value = valueAccessor();
-				value(v);
+				//var value = ko.utils.unwrapObservable(valueAccessor());
+				//ko.bindingHandlers.value.update(element, function () { return value; });
+				//var value = valueAccessor();
+				//value(v);
 			}
 		});
-	},
+	}/*,
 	update: function(element, valueAccessor) {
 		$(element).val(ko.utils.unwrapObservable(valueAccessor())).trigger('change');
-	}
+	}*/
 };
